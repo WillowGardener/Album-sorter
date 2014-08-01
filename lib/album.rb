6076c89cc5984d1e81478cfb742a306f8c21b2d3
Griffin Wimminger & Blake Mason <student@epicodus.com>
2014-08-01 10:36:57 -0700

    adds titles and artists to existing objects

--- lib/album.rb
+++ lib/album.rb
@@ -1,6 +1,24 @@
 class Album
+  @@albums = []
+
+  attr_reader :title, :artist
+
   def initialize attributes
     @title = attributes[:title]
     @artist = attributes[:artist]
+    @@albums << self
   end
+
+  def Album.albums
+    @@albums
+  end
+
+  def add_artist artist
+    @artist = {:artist => artist}
+  end
+
+  def add_title title
+    @title = {:title => title}
+  end
+
 end
--- spec/album_organizer_spec.rb
+++ spec/album_organizer_spec.rb
@@ -6,4 +6,27 @@ describe Album do
     new_album = Album.new ({:title => 'Warning', :artist => 'Greenday'})
     expect(new_album).to be_an_instance_of Album
   end
+
+  it "adds album to albums" do
+    new_album = Album.new ({:title => 'The Cost of Living', :artist => 'Jason Webley'})
+    expect(Album.albums[1]).to eq(new_album)
+  end
+
+  it "retrieves all albums" do
+    cd = Album.new ({:title => 'Jagged Little Pill'})
+    expect(Album.albums.length).to eq(3)
+  end
+
+  it "add an artist to an existing album" do
+    cd = Album.new ({:title => 'Jagged Little Pill'})
+    cd.add_artist("Alanis Morissette")
+    expect(cd.artist.fetch(:artist)).to eq("Alanis Morissette")
+  end
+
+  it "add an album title to an existing artist" do
+    cd = Album.new ({:artist => "Rilo Kiley"})
+    cd.add_title("Take Offs and Landings")
+    expect(cd.title.fetch(:title)).to eq("Take Offs and Landings")
+  end
+
 end
